dab4a138e021f286e1375b483588da9ab3285d1f
Maryan Genzhalo <genzhalo@gmail.com>
2023-01-27 16:31:42 +0200

    Improve load files

--- webapp/client/views/files/files.js
+++ webapp/client/views/files/files.js
@@ -1,27 +1,61 @@
 import { ReactiveVar } from "meteor/reactive-var";
 
 Template.files_list.onCreated(function () {
-  this.sort = { ceratedDate: 1 };
   this.isLoading = new ReactiveVar(false);
+  this.files = new ReactiveVar([]);
 });
 
 Template.files_list.onRendered(function () {
-  this.autorun(() => {
+  const limit = 60;
+  let skip = 0;
+  let isCanLoadMore = false;
+
+  this.load = () => {
+    const files = this.files.get();
+    if (this.isLoading.get() || files.length < skip) return;
     this.isLoading.set(true);
-    Meteor.subscribe(
-      "files.get",
-      { sort: this.sort },
-      {
-        onStop: (err) => {
-          if (err) showBootstrapGrowl(err.reason ?? err.message);
-          this.isLoading.set(false);
-        },
-        onReady: () => {
-          this.isLoading.set(false);
-        },
+    Meteor.call("files.get", { skip, limit }, (err, res) => {
+      if (res) {
+        const arr = files.concat(res);
+        skip += limit;
+        this.files.set(arr);
       }
-    );
+      this.isLoading.set(false);
+    });
+  };
+
+  const observer = new IntersectionObserver((entries) => {
+    const entry = entries[0];
+    isCanLoadMore = entry.isIntersecting;
+    if (isCanLoadMore) this.load();
+  }, {});
+
+  observer.observe(this.firstNode.querySelector(".footer"));
+
+  const observerResize = new MutationObserver(() => {
+    setTimeout(() => {
+      if (isCanLoadMore) this.load();
+    }, 100);
   });
+
+  observerResize.observe(this.firstNode.querySelector(".files-list-content"), {
+    childList: true,
+  });
+
+  this.deleteFile = (fileId) => {
+    Meteor.call("files.delete", [fileId], (err, res) => {
+      if (err) {
+        showBootstrapGrowl(err.reason ?? err.message);
+      } else {
+        const files = this.files.get();
+        const index = files.findIndex((file) => file._id == fileId);
+        if (index < 0) return;
+        files.splice(index, 1);
+        this.files.set(files);
+        skip -= 1;
+      }
+    });
+  };
 });
 
 Template.files_list.helpers({
@@ -35,15 +69,14 @@ Template.files_list.helpers({
   },
 
   files() {
-    const { sort } = Template.instance();
-    return Files.find({ creatorId: Meteor.userId() }, { sort });
+    return Template.instance().files.get();
   },
 });
 
 Template.files_list.events({
   "click .file-item_1[data-file-id]": (e, t) => {
     const fileId = e.currentTarget.dataset?.fileId;
-    const file = Files.findOne(fileId);
+    const file = t.files.get().find((file) => file._id == fileId);
     const { onClick } = t.data;
     if (file && typeof onClick === "function") {
       onClick(file);
@@ -53,9 +86,6 @@ Template.files_list.events({
   "click .file-item[data-file-id] .file-actions button.delete": (e, t) => {
     const el = e.target.closest(".file-item");
     const fileId = el.dataset?.fileId;
-    if (!fileId) return;
-    Meteor.call("files.delete", [fileId], (err, res) => {
-      if (err) showBootstrapGrowl(err.reason ?? err.message);
-    });
+    t.deleteFile(fileId);
   },
 });
--- webapp/server/publications/files.js
+++
@@ -1,6 +0,0 @@
-Meteor.publish({
-  "files.get": function ({ skip, limit, sort = { createdDate: 1 } } = {}) {
-    if (!this.userId) throw new Meteor.Error(401, "Unauthorized");
-    return Files.find({ creatorId: this.userId }, { limit, skip, sort });
-  },
-});
